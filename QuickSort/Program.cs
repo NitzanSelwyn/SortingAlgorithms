3fc56e86dde65b2bf721e153ed38355183d6b80a
Nitzan Selwyn <nitzans@sela.co.il>
2020-01-26 17:09:23 +0200

    xdasdasdas

---
+++ QuickSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 3, 1, 4, 6, 2, 3 };
+            var sort = new QuickSort();
+            sort.Sort(numbers);
+
+            Console.WriteLine(sort.ToString(numbers));
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+    class QuickSort
+    {
+
+        public void Sort(int[] arr)
+        {
+            Sort(arr, 0, arr.Length - 1);
+        }
+        private void Sort(int[] arr,int start,int end)
+        {
+            if (start >= end)
+                return;
+
+            var boundary = Partition(arr, start, end);
+
+            Sort(arr, start, boundary - 1);
+            Sort(arr, boundary + 1, end);
+        }
+
+        private int Partition(int[] arr, int start,int end)
+        {
+            var pivot = arr[end];
+            var boundry = start - 1;
+
+            for (int i = start; i <= end; i++)
+                if (arr[i] <= pivot)
+                    Sawp(arr, i, ++boundry);
+
+            return boundry;
+        }
+
+        private void Sawp(int[] arr, int index1, int index2)
+        {
+            var temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+
+        public string ToString(int[] arr)
+        {
+            var text = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                text += $"{arr[i]}, ";
+            }
+
+            return text;
+        }
+    }
+}
